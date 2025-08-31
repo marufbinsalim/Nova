5c4c400c9d9edd5906d057728c6c51e940035ba1
shaheen2013 <mdmarufbinsalim@gmail.com>
2025-08-31 14:22:03 +0600

    removed rapier

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import { Physics, RigidBody } from "@react-three/rapier";
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Box, OrbitControls } from "@react-three/drei";
 
-export default function App() {
+const Scene = () => {
+  const boxRef = useRef();
+  useFrame((state, delta) => {
+    boxRef.current.rotation.y += 0.02;
+  });
+
   return (
-    <Canvas camera={{ position: [6, 6, 6], fov: 50 }}>
-      {/* Lighting */}
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[10, 10, 5]} intensity={1} />
+    <>
+      <Box ref={boxRef} args={[1, 1, 1]} rotation={[0.5, 0, 0]}>
+        <meshNormalMaterial />
+      </Box>
+      <ambientLight />
+    </>
+  );
+};
 
-      {/* Controls */}
+const App = () => {
+  return (
+    <Canvas camera={{ fov: 70, position: [0, 0, 3] }}>
       <OrbitControls />
-
-      {/* Physics world */}
-      <Physics gravity={[0, -9.81, 0]}>
-        {/* Falling cube */}
-        <RigidBody>
-          <mesh position={[0, 5, 0]}>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color="royalblue" />
-          </mesh>
-        </RigidBody>
-
-        {/* Ground plane */}
-        <RigidBody type="fixed" colliders="cuboid">
-          <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
-            <planeGeometry args={[20, 20]} />
-            <meshStandardMaterial color="lightgreen" />
-          </mesh>
-        </RigidBody>
-      </Physics>
+      <Scene />
     </Canvas>
   );
-}
+};
+
+export default App;
